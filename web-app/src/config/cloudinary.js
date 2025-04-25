ac9688e8ad47aa1ee05a09fc8cb0081643c35cb7
ahmadazimi25 <ahmadazimi1025@gmail.com>
2025-04-25 12:13:25 -0400

    Add Cloudinary configuration and dependencies

---
+++ web-app/src/config/cloudinary.js
@@ -0,0 +1,11 @@
+import { Cloudinary } from '@cloudinary/url-gen';
+
+const cloudinary = new Cloudinary({
+  cloud: {
+    cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+    apiKey: process.env.REACT_APP_CLOUDINARY_API_KEY,
+    apiSecret: process.env.REACT_APP_CLOUDINARY_API_SECRET
+  }
+});
+
+export default cloudinary;
